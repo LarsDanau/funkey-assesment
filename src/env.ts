bf7214f90a1496c4cd1f5a0cf596598027adc71f
LarsD-Ideal <larsdanauhome@gmail.com>
2025-06-11 00:00:31 +0200

    Use zod/v4

--- src/env.ts
+++ src/env.ts
@@ -1,6 +1,6 @@
 import { createEnv } from "@t3-oss/env-nextjs";
 import { config } from "dotenv";
-import { z } from "zod";
+import { z } from "zod/v4";
 config({ path: ".env" });
 
 export const env = createEnv({
